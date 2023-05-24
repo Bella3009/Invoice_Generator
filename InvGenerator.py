3cec88044697537551415864c51ac9bc294b9771
Bella3009 <120841781+Bella3009@users.noreply.github.com>
2023-05-24 20:57:05 +0200

    Update InvGenerator.py
    Added the date in the Invoice PDF

--- InvGenerator.py
+++ InvGenerator.py
@@ -8,14 +8,19 @@ filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
+    
     # Retrieve the data for the PDF
     filename = Path(filepath).stem
-    # Only the first part of the name is the invoice number
-    inv_nr = filename.split("-")[0]
+    # Split will give 2 variables therefore like this the variables will be set
+    inv_nr, date = filename.split("-")
+    
     # Input the data in the PDF
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice nr.{inv_nr}")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr.{inv_nr}", ln=1)
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Date: {date}")
+    
     pdf.output(f"PDFs/{filename}.pdf")
-
