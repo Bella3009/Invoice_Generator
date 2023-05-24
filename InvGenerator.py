454660b7d3b3caaf754307a7da371cf0bb08322f
Bella3009 <120841781+Bella3009@users.noreply.github.com>
2023-05-24 21:26:03 +0200

    Update InvGenerator.py
    Add the table in the PDF

--- InvGenerator.py
+++ InvGenerator.py
@@ -7,8 +7,6 @@ from pathlib import Path
 filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
-    
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
     
@@ -21,6 +19,30 @@ for filepath in filepaths:
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=50, h=8, txt=f"Invoice nr.{inv_nr}", ln=1)
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Date: {date}")
+    pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=1)
     
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    # Set the table header
+    excel_columns = df.columns
+    excel_columns = [item.replace("_", " ").title() for item in excel_columns]
+    
+    pdf.set_font(family="Times", size=16)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8,txt=excel_columns[0], border=1)
+    pdf.cell(w=70, h=8,txt=excel_columns[1],border=1)
+    pdf.cell(w=30, h=8,txt=excel_columns[2], border=1)
+    pdf.cell(w=30, h=8,txt=excel_columns[3], border=1)
+    pdf.cell(w=30, h=8,txt=excel_columns[4], border=1, ln=1)
+    
+    # Set row data
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=16)
+        pdf.set_text_color(80, 80, 80)
+        # Use the string function because integer value cause an error
+        pdf.cell(w=30, h=8,txt=str(row["product_id"]), border=1)
+        pdf.cell(w=70, h=8,txt=str(row["product_name"]),border=1)
+        pdf.cell(w=30, h=8,txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=8,txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8,txt=str(row["total_price"]), border=1, ln=1)
+        
     pdf.output(f"PDFs/{filename}.pdf")
